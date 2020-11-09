6fee18e064320876787fbf793630cf4294a7b083
Alvin Chen <chenbog@bc.edu>
2020-11-08 19:20:22 -0500

    Initial Commit

---
+++ Snacktacular/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Snacktacular
+//
+//  Created by Alvin Chen on 11/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
